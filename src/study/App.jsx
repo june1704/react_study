34c0b9a15de4370b4d98bc2999ed695a399fd36b
june1704 <xvz1704@naver.com>
2025-01-14 14:05:35 +0900

    first commit

---
+++ src/study/App.jsx
@@ -0,0 +1,17 @@
+import Hello from "./Hello/Hello";
+import Hello2 from "./Hello2/Hello2";
+import Hello3 from "./hello3/Hello3";
+import Hello4 from "./Hello4/Hello4";
+
+function App() {
+    return (
+        <>
+            <Hello />
+            <Hello2 />
+            <Hello3 />
+            <Hello4 />
+        </>
+    )
+}
+
+export default App;
---
+++ src/study/Hello/Hello.jsx
@@ -0,0 +1,9 @@
+
+function Hello() {
+    return <>
+        <h1>Hello react!!</h1>
+        <input type="text" />
+    </> // React.Fragment
+}
+
+export default Hello;
